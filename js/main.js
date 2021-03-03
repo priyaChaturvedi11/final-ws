f4445c58150a6a78104a7fe800d80ed4acd9303e
gama626 <55777739+gama626@users.noreply.github.com>
2021-03-03 14:24:11 +0530

    Events and Past Workshop Added

--- js/main.js
+++ js/main.js
@@ -43,3 +43,29 @@ $(function () {
     bodyElement.prepend(header);
     bodyElement.append(footer);
 });
+
+
+/* ----------------------------------------------------------- */
+	/*  12. SCROLL UP BUTTON
+	/* ----------------------------------------------------------- */
+jQuery(function($){
+	//Check to see if the window is top if not then display button
+
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 300) {
+      $('.scrollToTop').fadeIn();
+    } else {
+      $('.scrollToTop').fadeOut();
+    }
+  });
+   
+  //Click event to scroll to top
+
+  $('.scrollToTop').click(function(){
+    $('html, body').animate({scrollTop : 0},800);
+    return false;
+  });
+
+
+
+});
